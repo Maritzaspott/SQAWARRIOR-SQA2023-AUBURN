3c557d320a0f1beae4a6caf0f08864c5e54b5695
Maritza Spott <Maritzaspott@gmail.com>
2023-05-01 22:57:57 -0500

    Update main.py

--- KubeSec-master/main.py
+++ KubeSec-master/main.py
@@ -64,6 +64,9 @@ if __name__ == '__main__':
     # ORG_DIR         = '/Users/arahman/K8S_REPOS/TEST_REPOS/'
     # OUTPUT_FILE_CSV = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/Kubernetes/StaticTaint/data/V16_TEST_OUTPUT.csv'
 
+    ORG_DIR         = '/Users/marit/Documents/(Year 4)/Spring 2023/COMP 5710/Project/SQAWARRIOR-SQA2023-AUBURN/'
+    OUTPUT_FILE_CSV = '/Users/marit/Documents/(Year 4)/Spring 2023/COMP 5710/Project/SQAWARRIOR-SQA2023-AUBURN/main_output.csv'
+
     content_as_ls   = scanner.runScanner( ORG_DIR )
     df_all          = pd.DataFrame( getCountFromAnalysis( content_as_ls ) )
 
